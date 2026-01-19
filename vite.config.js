14ec028ada63bbe9aa1f16619510adb463887fb7
CharanSala <salacharan81@gmail.com>
2026-01-19 18:07:57 +0530

    Ready to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Portifolio/",
+  base: "/portfolio/", // <-- Add this line (your repo name)
 });
